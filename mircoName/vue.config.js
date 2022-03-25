b78347640228bc3db66ca2460a14401658af3d0d
tobyjwt <jin_wentao@qq.com>
2022-03-25 15:12:50 +0800

    auto commit mircoName

--- mircoName/vue.config.js
+++ mircoName/vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 
 const packageName = require('./package.json').name
-const version = require('./package.json').version
+const deployId = require('./package.json').deployId
 
 module.exports = defineConfig({
   // publicPath: `https://cdn-app.myshopline.com/web/mirco/${packageName}`,
-  publicPath: `https://cdn-app.myshopline.com/web/mirco/${packageName}/${version}`,
+  publicPath: `https://cdn-app.myshopline.com/web/mirco/${packageName}/${deployId}`,
   transpileDependencies: true,
   devServer: {
     headers: {
